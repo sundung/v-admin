7db37698ec0bef6f243d29cfd18b47bfa4fc6ad1
孙志强 <1181288394@qq.com>
2021-01-06 23:51:12 +0800

    商品列表-table表格渲染完成

--- src/components/goods/List.vue
+++ src/components/goods/List.vue
@@ -1,10 +1,117 @@
 <template>
-  <div>list</div>
+  <div>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图区域 -->
+    <el-card>
+      <!-- 搜索框 -->
+      <el-row :gutter="20">
+        <el-col :span="12">
+          <el-input v-model="queryInfo.query"
+                    placeholder="请输入内容"
+                    class="input-with-select"
+                    clearable>
+            <el-button slot="append"
+                       icon="el-icon-search"></el-button>
+          </el-input>
+        </el-col>
+        <el-col :span="6">
+          <el-button type="primary">添加商品</el-button>
+        </el-col>
+      </el-row>
+      <!-- 商品表格 -->
+      <el-table :data="goodsList"
+                border
+                stripe>
+        <el-table-column type="index"></el-table-column>
+        <el-table-column label="商品名称"
+                         prop="goods_name"></el-table-column>
+        <el-table-column label="商品价格(元)"
+                         prop="goods_price"
+                         width="100px"></el-table-column>
+        <el-table-column label="商品重量"
+                         width="70px"
+                         prop="goods_weight"></el-table-column>
+        <el-table-column label="创建时间"
+                         width="140px"
+                         prop="add_time">
+          <template slot-scope="scope">
+            {{scope.row.add_time | dataFormat}}
+          </template>
+        </el-table-column>
+        <el-table-column label="操作"
+                         width="130px">
+          <template>
+            <el-button type="primary"
+                       size="mini"
+                       icon="el-icon-edit"></el-button>
+            <el-button type="danger"
+                       size="mini"
+                       icon="el-icon-delete"></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <!-- 分页区域 -->
+      <el-pagination @size-change="handleSizeChange"
+                     @current-change="handleCurrentChange"
+                     :current-page="queryInfo.pagenum"
+                     :page-sizes="[2,5,10]"
+                     :page-size="queryInfo.pagesize"
+                     layout="total, sizes, prev, pager, next, jumper"
+                     :total="total">
+      </el-pagination>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      // 查询参数对象
+      queryInfo: {
+        // 查询条件
+        query: '',
+        // 当前显示页数
+        pagenum: 1,
+        // 每页显示的条数
+        pagesize: 10
+      },
 
+      // 商品列表数据
+      goodsList: [],
+
+      // 总数据条数
+      total: 0
+    }
+  },
+  created() {
+    this.getGoodsList()
+  },
+  methods: {
+    async getGoodsList() {
+      const { data: res } = await this.$http.get('goods', { params: this.queryInfo })
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取商品列表失败')
+      }
+      this.$message.success('获取商品列表成功')
+      this.goodsList = res.data.goods
+      this.total = res.data.total
+    },
+    handleSizeChange(newSize) {
+      this.queryInfo.pagesize = newSize
+      this.getGoodsList()
+    },
+
+    handleCurrentChange(newPage) {
+      this.queryInfo.pagenum = newPage
+      this.getGoodsList()
+    }
+  }
 }
 </script>
 
