efcb2f3216c34a2d0f7c328787eda70ff35b5e16
孙志强 <1181288394@qq.com>
2021-01-07 18:45:12 +0800

    订单管理-订单列表渲染完成

--- src/components/order/Order.vue
+++ src/components/order/Order.vue
@@ -6,22 +6,119 @@
       <el-breadcrumb-item>订单管理</el-breadcrumb-item>
       <el-breadcrumb-item>订单列表</el-breadcrumb-item>
     </el-breadcrumb>
-    <!-- 搜索框区域 -->
-    <el-row>
-      <el-col :span="8">
-        <el-input placeholder="请输入内容"
-                  clearable>
-          <el-button slot="append"
-                     icon="el-icon-search"></el-button>
-        </el-input>
-      </el-col>
-    </el-row>
+
+    <!-- 卡片视图区域 -->
+    <el-card>
+      <!-- 搜索框区域 -->
+      <el-row>
+        <el-col :span="8">
+          <el-input placeholder="请输入内容"
+                    clearable>
+            <el-button slot="append"
+                       icon="el-icon-search"></el-button>
+          </el-input>
+        </el-col>
+      </el-row>
+      <!-- 订单列表表格 -->
+      <el-table :data="ordersList"
+                border
+                stripe>
+        <el-table-column type="index"></el-table-column>
+        <el-table-column label="订单编号"
+                         prop="order_number"></el-table-column>
+        <el-table-column label="订单价格"
+                         prop="order_price"
+                         width="100px"></el-table-column>
+        <el-table-column label="是否付款"
+                         width="100px"
+                         prop="pay_status">
+          <template slot-scope="scope">
+            <el-tag type='success'
+                    v-if="scope.row.pay_status === '1'">已付款</el-tag>
+            <el-tag type='danger'
+                    v-else>未付款</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column label="是否发货"
+                         width="70px"
+                         prop="is_send"></el-table-column>
+        <el-table-column label="下单时间"
+                         prop="create_time">
+          <template slot-scope="scope">
+            {{scope.row.create_time | dataFormat}}
+          </template>
+        </el-table-column>
+        <el-table-column label="操作"
+                         width="120px">
+          <template>
+            <el-button type="primary"
+                       size="mini"
+                       icon="el-icon-edit"></el-button>
+            <el-button type="success"
+                       size="mini"
+                       icon="el-icon-location"></el-button>
+          </template></el-table-column>
+      </el-table>
+      <!-- 分页区域 -->
+      <el-pagination @size-change="handleSizeChange"
+                     @current-change="handleCurrentChange"
+                     :current-page="queryInfo.pagenum"
+                     :page-sizes="[2,5,10]"
+                     :page-size="queryInfo.pagesize"
+                     layout="total, sizes, prev, pager, next, jumper"
+                     :total="total">
+      </el-pagination>
+    </el-card>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      // 查询参数对象
+      queryInfo: {
+        // 查询条件
+        query: '',
+        // 当前显示页数
+        pagenum: 1,
+        // 每页显示的条数
+        pagesize: 10
+      },
 
+      // 订单列表数据
+      ordersList: [],
+
+      // 总页数
+      total: 0
+    }
+  },
+  created() {
+    this.getOrderList()
+  },
+  methods: {
+
+    async getOrderList() {
+      const { data: res } = await this.$http.get('orders', { params: this.queryInfo })
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取商品列表失败')
+      }
+      this.$message.success('获取商品列表成功')
+      this.ordersList = res.data.goods
+      this.total = res.data.total
+      console.log(this.ordersList)
+    },
+
+    handleSizeChange(newSize) {
+      this.queryInfo.pagesize = newSize
+      this.getOrderList()
+    },
+
+    handleCurrentChange(newPage) {
+      this.queryInfo.pagenum = newPage
+      this.getOrderList()
+    }
+  }
 }
 </script>
 
