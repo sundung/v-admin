777eb49c9e7f0ab35a46a4d5415d596825a461e5
孙志强 <1181288394@qq.com>
2021-01-07 19:43:41 +0800

    更新 element-ui 版本,全部引入方式

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-// import './plugins/element.js'
+// 导入 elementui
 import ElementUI from 'element-ui'
 import locale from 'element-ui/lib/locale/lang/en'
 import 'element-ui/lib/theme-chalk/index.css'
